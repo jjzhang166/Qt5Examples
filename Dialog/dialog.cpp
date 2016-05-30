63319e2e7076ee9b87359ec02ee452e70224629e
gitjf <jackiefq@163.com>
2016-05-31 00:39:49 +0800

    对半径输入框中的输入进行检查，以确保只接收数字输入，其他字符将导致输入重置！

--- Dialog/dialog.cpp
+++ Dialog/dialog.cpp
@@ -26,10 +26,30 @@ void AreaCounter::on_radiusLabelEdit_textChanged(const QString &arg1)
     bool ok;
     QString tempStr;
     QString valueStr=ui->radiusLabelEdit->text();
-    //int valueInt=valueStr.toInt(&ok);
-    //double area=valueInt*valueInt*PI;
 
-    double valueDouble=valueStr.toDouble(&ok);
-    double area=valueDouble*valueDouble*PI;
-    ui->areaLabel_2->setText(tempStr.setNum(area));
+    QByteArray strByteArray = valueStr.toLatin1();
+    const char *chString = strByteArray.data();
+    int dotCount=0;
+    while (*chString && *chString>='0' && *chString<='9' || *chString=='.')
+    {
+            if(dotCount<2)
+                chString++;
+            else
+                break;
+
+            if(*chString=='.') dotCount+=1;
+    }
+
+    if (*chString)
+    {//not a digit,reset the number!
+        ui->radiusLabelEdit->setText("0.0");
+        ui->areaLabel_2->setText("0.0");
+    }
+    else
+    {//is a digit,show the result!
+        double valueDouble=valueStr.toDouble(&ok);
+        double area=valueDouble*valueDouble*PI;
+        ui->areaLabel_2->setText(tempStr.setNum(area));
+    }
 }
+
