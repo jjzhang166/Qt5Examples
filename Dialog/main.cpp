f1019bf5d15bb5d196d40d4d600c4d4fc4e016c0
gitjf <Jackiefq@163.COM>
2016-05-31 18:11:37 +0800

    no message

--- Dialog/main.cpp
+++ Dialog/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     AreaCounter w;
     //禁止窗口最大化
-    w.setWindowFlags(w.windowFlags()&~Qt::WindowMaximizeButtonHint&~Qt::WindowMaximized);
+    w.setWindowFlags( w.windowFlags()&~Qt::WindowMaximizeButtonHint&~Qt::WindowMaximized );
     //固定窗口大小
     w.setFixedSize(294,91);
     w.show();
